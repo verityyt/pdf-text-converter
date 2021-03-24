a6cceabbec81dfb3c6ff18fb6af2546ad9358bd5
Verity <joshua-schmidt@online.de>
2021-03-24 21:14:47 +0100

    :construction: Change size of converted images (from pdf) to 638x825 (perfect for ui)

--- src/main/kotlin/utils/ImageConverter.kt
+++ src/main/kotlin/utils/ImageConverter.kt
@@ -2,6 +2,7 @@ package utils
 
 import org.ghost4j.document.PDFDocument
 import org.ghost4j.renderer.SimpleRenderer
+import java.awt.image.BufferedImage
 import java.awt.image.RenderedImage
 import java.io.File
 import javax.imageio.ImageIO
@@ -27,7 +28,14 @@ object ImageConverter {
         folder.mkdir()
 
         for (i in images.indices) {
-            ImageIO.write(images[i] as RenderedImage, "png", File(("output//page${i + 1}").toString() + ".png"))
+            val image = images[i]
+
+            val resizedImage = BufferedImage(638, 825, BufferedImage.TYPE_INT_RGB)
+            val graphics2D = resizedImage.createGraphics()
+            graphics2D.drawImage(image, 0, 0, 638, 825, null)
+            graphics2D.dispose()
+
+            ImageIO.write(resizedImage as RenderedImage, "png", File(("output//page${i + 1}").toString() + ".png"))
         }
 
     }
