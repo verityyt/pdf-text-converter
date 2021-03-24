58dd8cdd0fc88fcdcd7d2675307dffc86e342729
Verity <joshua-schmidt@online.de>
2021-03-24 15:22:29 +0100

    :heavy_plus_sign: Add ghost4j dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,9 +8,19 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven {
+        name = "org.ghost4j.repository.releases"
+        url = uri("http://repo.ghost4j.org/maven2/releases")
+    }
+    maven {
+        name = "org.ghost4j.repository.snapshots"
+        url = uri("http://repo.ghost4j.org/maven2/snapshots")
+    }
 }
 
 dependencies {
+    implementation("org.ghost4j:ghost4j:1.0.0")
+
     implementation(kotlin("stdlib"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
