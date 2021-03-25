2a8859a8588ded7cdd0e533fd91786f91141349f
Verity <joshua-schmidt@online.de>
2021-03-25 21:44:01 +0100

    :lipstick: Update look and feel of FileChooser (windows theme now)

--- src/main/kotlin/userinterface/utils/FileChooser.kt
+++ src/main/kotlin/userinterface/utils/FileChooser.kt
@@ -3,6 +3,7 @@ package userinterface.utils
 import java.io.File
 import javax.swing.JFileChooser
 import javax.swing.JFrame
+import javax.swing.SwingUtilities
 import javax.swing.UIManager
 import javax.swing.filechooser.FileNameExtensionFilter
 
@@ -10,12 +11,12 @@ object FileChooser {
 
     fun open(): File? {
 
+        UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel")
+
         val chooser = JFileChooser()
         chooser.currentDirectory = File(System.getProperty("user.home") + "\\Desktop")
         chooser.fileSelectionMode = JFileChooser.FILES_ONLY
         chooser.fileFilter = FileNameExtensionFilter("Portable Document Format (.pdf)", "pdf")
-
-        UIManager.setLookAndFeel(UIManager.getLookAndFeel())
 
         val choice = chooser.showDialog(JFrame(), "Select PDF file")
 
