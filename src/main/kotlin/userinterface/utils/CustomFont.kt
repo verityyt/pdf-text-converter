22be9c9272fb5841fdc889205d8fb447e293032e
Verity <joshua-schmidt@online.de>
2021-03-25 14:41:39 +0100

    :sparkles: Introduce CustomFont

---
+++ src/main/kotlin/userinterface/utils/CustomFont.kt
@@ -0,0 +1,31 @@
+package userinterface.utils
+
+import java.awt.*
+import java.io.File
+
+object CustomFont {
+
+    var regular: Font? = null
+    var light: Font? = null
+
+    fun registerFonts() {
+        registerRegular()
+        registerLight()
+    }
+
+
+    private fun registerRegular() {
+        regular =
+            Font.createFont(Font.TRUETYPE_FONT, File("files\\fonts\\Product-Sans-Regular.ttf"))
+        val ge = GraphicsEnvironment.getLocalGraphicsEnvironment()
+        ge.registerFont(regular)
+    }
+
+    private fun registerLight() {
+        light =
+            Font.createFont(Font.TRUETYPE_FONT, File("files\\fonts\\Product-Sans-Light.ttf"))
+        val ge = GraphicsEnvironment.getLocalGraphicsEnvironment()
+        ge.registerFont(light)
+    }
+
+}
