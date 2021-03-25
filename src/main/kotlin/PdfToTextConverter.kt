132d6cf694b4a05c92f9194d7b7423b247159f85
Verity <joshua-schmidt@online.de>
2021-03-25 21:49:09 +0100

    :bug: Add error if software runs not on windows

--- src/main/kotlin/PdfToTextConverter.kt
+++ src/main/kotlin/PdfToTextConverter.kt
@@ -1,5 +1,8 @@
 import userinterface.Userinterface
+import java.awt.BorderLayout
+import java.awt.Dimension
 import java.io.File
+import javax.swing.*
 
 object PdfToTextConverter {
 
@@ -8,11 +11,30 @@ object PdfToTextConverter {
 
     @JvmStatic
     fun main(args: Array<String>) {
-
         // Requires Ghostscript
 
-        Userinterface.open()
+        if(checkOs()) {
+            Userinterface.open()
+        }
+    }
 
+    fun checkOs(): Boolean {
+        val operatingSystem = System.getProperty("os.name")
+
+        if(operatingSystem != "Windows 10") {
+
+            val dialog = JDialog()
+            dialog.add(JLabel("     This software only supports Windows 10 (64/32bit"))
+
+            dialog.title = "Error"
+            dialog.size = Dimension(350,100)
+            dialog.isVisible = true
+
+            return false
+
+        }
+
+        return true
     }
 
 }
