ea6205038b577ef62755d93fd98eb3d6b669e3c4
Verity <joshua-schmidt@online.de>
2021-03-25 15:08:30 +0100

    :sparkles: Add function-parameter which is executed after converting (ImageConverter)

--- src/main/kotlin/utils/ImageConverter.kt
+++ src/main/kotlin/utils/ImageConverter.kt
@@ -11,43 +11,45 @@ import javax.imageio.ImageIO
 
 object ImageConverter {
 
-    fun convert(file: File) {
+    fun convert(file: File, exec: () -> Unit) {
+        Thread {
+            Logger.info("Converting ${file.name} to images...")
 
-        Logger.info("Converting ${file.name} to images...")
+            Logger.debug("Loading document...")
 
-        Logger.debug("Loading document...")
+            val doc = PDFDocument()
+            doc.load(file)
 
-        val doc = PDFDocument()
-        doc.load(file)
+            Logger.debug("Rendering document...")
 
-        Logger.debug("Rendering document...")
+            val renderer = SimpleRenderer()
+            renderer.resolution = 300
 
-        val renderer = SimpleRenderer()
-        renderer.resolution = 300
+            val images = renderer.render(doc)
 
-        val images = renderer.render(doc)
+            Logger.debug("Converted document to images!")
+            Logger.debug("Saving converted images to temp folder...")
 
-        Logger.debug("Converted document to images!")
-        Logger.debug("Saving converted images to temp folder...")
+            val folder = File("${PdfToTextConverter.temp.absolutePath}\\converted\\")
 
-        val folder = File("${PdfToTextConverter.temp.absolutePath}\\converted\\")
+            folder.deleteRecursively()
+            folder.mkdirs()
 
-        folder.deleteRecursively()
-        folder.mkdirs()
+            for (i in images.indices) {
+                val image = images[i]
 
-        for (i in images.indices) {
-            val image = images[i]
+                val resizedImage = BufferedImage(638, 825, BufferedImage.TYPE_INT_RGB)
+                val graphics2D = resizedImage.createGraphics()
+                graphics2D.drawImage(image, 0, 0, 638, 825, null)
+                graphics2D.dispose()
 
-            val resizedImage = BufferedImage(638, 825, BufferedImage.TYPE_INT_RGB)
-            val graphics2D = resizedImage.createGraphics()
-            graphics2D.drawImage(image, 0, 0, 638, 825, null)
-            graphics2D.dispose()
+                ImageIO.write(resizedImage as RenderedImage, "png", File(("${folder.absolutePath}\\page${i + 1}").toString() + ".png"))
+            }
 
-            ImageIO.write(resizedImage as RenderedImage, "png", File(("${folder.absolutePath}\\page${i + 1}").toString() + ".png"))
-        }
+            Logger.info("Successfully converted document to images!")
 
-        Logger.info("Successfully converted document to images!")
-
+            exec()
+        }.start()
     }
 
 }
